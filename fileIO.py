592e4416a682027ee38e652643877118c3e4bce1
zzklove3344 <zzklove3344@hotmail.com>
2019-03-01 12:07:14 +0800

    add two functions
    add two functions, write_txt_file and read_txt_file

--- fileIO.py
+++ fileIO.py
@@ -4,6 +4,41 @@
 
 from divideSegments import ECGDataSegment
 from constant import SEGMENTS_BASE_PATH, SEGMENTS_NUMBER_TRAIN, SEGMENTS_NUMBER_TEST
+from common import my_random_func
+
+
+def write_txt_file(list_info, write_file_path):
+	"""
+	Write list object to TXT file.
+	:param list list_info: List object you want to write.
+	:param string write_file_path: TXT file path.
+	:return: None
+	"""
+	with open(write_file_path, "w") as f:
+		for info in list_info:
+			f.write(str(info) + "\n")
+
+
+def read_txt_file(read_file_path):
+	"""
+	Read TXT file to list object.
+	:param string read_file_path: TXT file path.
+	:return list: list object
+	"""
+	with open(read_file_path) as f:
+		list_t = []
+		lines = f.readlines()
+		for line in lines:
+			float_list = []
+			str_t = line.replace("[", "").replace("]", "").replace("\n", "").split(",")
+			for str_s in str_t:
+				if str_s != "":
+					float_list.append(float(str_s))
+				else:
+					print(read_file_path)
+			list_t.append(float_list)
+	
+	return list_t
 
 
 def get_database(database_name, is_debug=False):
@@ -30,7 +65,8 @@ def get_database(database_name, is_debug=False):
 			eds = ECGDataSegment()
 			eds.global_id = segment_number
 			eds.read_ecg_segment(read_path)
-			# if len(eds.data) == 6000:
+			# read_wavelet_decomposition(eds)
+			
 			database.append(eds)
 		if is_debug:
 			print("length of database: %s" % len(database))
@@ -51,7 +87,29 @@ def get_database(database_name, is_debug=False):
 			path = SEGMENTS_BASE_PATH + "test/"
 			test_set = get_ecg_segments(path, SEGMENTS_NUMBER_TEST)
 			return train_set, test_set
-		
+
+
+def get_some_ecg_segments(number):
+	"""
+	Return some ecg samples for debug.
+	:param int number: the number of segments you want to sample from all ecg segments.
+	:return list: ecg segments.
+	"""
+	
+	random_list = my_random_func(number, 0, SEGMENTS_NUMBER_TRAIN)
+	
+	sample_segments = []
+	for value in random_list:
+		print("read %s" % value)
+		eds = ECGDataSegment()
+		eds.global_id = value
+		eds.read_ecg_segment("train")
+		sample_segments.append(eds)
+	return sample_segments
+
+
+
+
 		
 if __name__ == '__main__':
 	print("test statement")
